2304149748bf4007a7f7cb6577c6f780e48df142
JoaoRicardoBSouza <joaoricardobsouza@gmail.com>
2026-02-26 15:20:58 -0300

    fix

--- src/main/java/com/bs/school/models/AlunoModel.java
+++ src/main/java/com/bs/school/models/AlunoModel.java
@@ -1,9 +1,11 @@
 package com.bs.school.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class AlunoModel {
 
     @Id
